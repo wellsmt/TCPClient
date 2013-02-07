39cdde2b77b3e178d210ee0eb38b1c23e613794e
edrinkercope <marcbernardini@gmail.com>
2013-02-06 17:33:35 -0700

    Remove synch list

--- src/com/lp/io/DataInterpreter.java
+++ src/com/lp/io/DataInterpreter.java
@@ -1,19 +1,19 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
+// Copyright Marc Bernardini 2013
 package com.lp.io;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 /**
- *
+ * The DataInterpreter produces messages using data received from 
+ *  a source. Typically the source is a Socket but can be anything
+ *  that produces bytes of data.
  * @author marc
  */
 public class DataInterpreter implements MessageProducer {
-   private ArrayList<MessageConsumer> observers = new ArrayList<MessageConsumer>();
+   private List<MessageConsumer> observers = new ArrayList<MessageConsumer>();
 
    public void addRawData(byte[] buffer){
       System.out.println(buffer);
